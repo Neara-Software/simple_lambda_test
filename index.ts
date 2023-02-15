a08ac2f7a3f8bd0e4d64a23aa2b02d8538e3882d
az <a-z1997@hotmail.com>
2023-02-16 00:38:47 +1100

    create igw and natgw

--- index.ts
+++ index.ts
@@ -4,12 +4,24 @@ import * as awsx from "@pulumi/awsx";
 
 const stack = pulumi.getStack();
 
-// Create VPC and private subnet
-const vpc = new aws.ec2.Vpc("andy-vpc", { cidrBlock: "10.0.0.0/16" });
+const tags = { Name: "neara-task" };
 
-const subnet = new aws.ec2.Subnet("private-subnet", {
+// Create VPC and private subnet
+const vpc = new aws.ec2.Vpc("andy-vpc", {
+  cidrBlock: "10.0.0.0/16",
+  tags: tags,
+});
+
+const privateSubnet = new aws.ec2.Subnet("private-subnet", {
   vpcId: vpc.id,
   cidrBlock: "10.0.1.0/24",
+  tags: tags,
+});
+
+const publicSubnet = new aws.ec2.Subnet("public-subnet", {
+  vpcId: vpc.id,
+  cidrBlock: "10.0.2.0/24",
+  tags: tags,
 });
 
 // Lambda definitions
@@ -17,6 +29,7 @@ const lambdaRole = new aws.iam.Role("info-handler-role", {
   assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(
     aws.iam.Principals.LambdaPrincipal
   ),
+  tags: tags,
 });
 
 const infoHandler = new aws.lambda.Function("info-handler", {
@@ -24,6 +37,7 @@ const infoHandler = new aws.lambda.Function("info-handler", {
   runtime: aws.lambda.Runtime.Python3d9,
   code: new pulumi.asset.FileArchive("./lambda/main.py.zip"),
   handler: "main.info_handler", // fileName.methodName
+  tags: tags,
 });
 
 // API Gateway
@@ -38,15 +52,18 @@ const securityGroup = new aws.ec2.SecurityGroup("api-gateway-security-group", {
       cidrBlocks: ["0.0.0.0/0"],
     },
   ],
+  tags: tags,
 });
 
 const vpcLink = new aws.apigatewayv2.VpcLink("andy-vpc-link", {
-  subnetIds: [subnet.id],
+  subnetIds: [privateSubnet.id],
   securityGroupIds: [securityGroup.id],
+  tags: tags,
 });
 
 const api = new aws.apigatewayv2.Api("andy-api-gateway", {
   protocolType: "HTTP",
+  tags: tags,
 });
 
 const withApi = new aws.lambda.Permission(
@@ -64,7 +81,7 @@ const integration = new aws.apigatewayv2.Integration("andy-integration", {
   apiId: api.id,
   integrationType: "AWS_PROXY",
   integrationUri: infoHandler.arn,
-  //integrationMethod: "GET",
+  integrationMethod: "GET",
 });
 
 const route = new aws.apigatewayv2.Route("get-info-route", {
@@ -79,7 +96,24 @@ const stage = new aws.apigatewayv2.Stage("api-stage", {
   autoDeploy: true,
 });
 
+// Create internet gateway, nat gateway and edit route tables
+const igw = new aws.ec2.InternetGateway("igw", {
+  vpcId: vpc.id,
+  tags: tags,
+});
+
+const natEip = new aws.ec2.Eip("nat-eip", {
+  vpc: true,
+  tags: tags,
+});
+
+const natgw = new aws.ec2.NatGateway("natgw", {
+  subnetId: publicSubnet.id,
+  allocationId: natEip.id,
+  tags: tags,
+});
+
 // Log info
-export const subnetCidr = subnet.cidrBlock;
+export const subnetCidr = privateSubnet.cidrBlock;
 export const lambdaFunctionArn = infoHandler.arn;
 export const endpoint = api.apiEndpoint;
