fa0c1efccd462bcd7f15bb0ad78f016b6d47b681
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2024-03-08 15:53:07 -0300

    run linter fix

--- lambda/main.py
+++ lambda/main.py
@@ -1,16 +1,13 @@
 import json
 import os
 
+
 def info_handler(event, context):
-    json_region = os.environ['AWS_REGION']
+    json_region = os.environ["AWS_REGION"]
     print(event)
     print(context)
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "Region ": json_region
-        })
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"Region ": json_region}),
     }
